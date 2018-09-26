81b14c96326bea65e0f2589a0aa0a2465e4d5e84
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-09-26 17:11:10 -0500

    basic node app

---
+++ count.js
@@ -0,0 +1,11 @@
+exports.numbersFunctionUp = function() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
+}
+
+exports.numbersFunctionDown = function() {
+  for (var i = 5; i >= 1; i--) {
+    console.log(i);
+  }
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+const greetings = require("./greeting.js");
+const count = require("./count.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic ();
+
+count.numbersFunctionUp ();
+
+count.numbersFunctionDown ();
